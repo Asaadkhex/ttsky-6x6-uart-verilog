82392c8bb5413048a445bcc654a2e302ae2a65fa
Asaadkhex <asaad@hexabitz.com>
2026-07-16 15:46:50 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -211,7 +211,7 @@ module tt_um_Asaadkhex_6x6u (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
+  
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   switch6x6_nand switch6x6 (
     .x  (ui_in[6:1]),
@@ -223,12 +223,14 @@ module tt_um_Asaadkhex_6x6u (
     .sel5  (3'b000),
     .y  (uo_out[6:1])
   );  
+
+  // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
   assign uio_oe  = 0;
-  assign uio_out [0]  = 0;
-  assign uio_out [7]  = 0;
+  assign uo_out [0]  = 0;
+  assign uo_out [7]  = 0;
 
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena, clk, rst_n, ui_in[0], ui_in[7], 1'b0};
+  wire _unused = &{ena, clk, rst_n, ui_in[0], ui_in[7], 1'b0};
 
 endmodule
